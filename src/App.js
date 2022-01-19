c9f51461346f9ab4e6f5fc7f9e40041e09064966
shalini Kumarii <bittukumari@gmail.com>
2022-01-19 11:46:37 -0800

    Pokemon api first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+
+  const [pokemons, setPokemones] = useState([])
+
+  // useEffect to show the data at page loding
+  // useEffect(() =>{
+  //   fetchPokemons();
+  // },[]) // //never put the SAME state variable in [] bc useEffect will re-trigger
+
+  // Vanilla JS fetch
+  
+  const fetchPokemons = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(res => {
+        return res.json();
+      })
+      .then(jsonRes => {
+        console.log(jsonRes);
+        setPokemones(jsonRes.results);
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>pokemon</h2>
+    <button onClick={fetchPokemons}>Fetch Pokemon</button> 
+    <hr />
+   {/*{JSON.stringify(pokemons)}*/}
+    <table>
+    <thead>
+      <tr>
+      <th>Count</th>
+        <th>Pokemon Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+          pokemons.map((pok,index) => {
+            return (
+              <tr key={index}>
+                <td>{index}</td>
+                <td>{pok.name}</td>
+              </tr>
+            )
+          }) 
+      }
+    </tbody>
+  </table> 
     </div>
   );
 }
